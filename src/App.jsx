17c1141cbfd00a881f0de5f96788cf1ed180d4cf
Avinash Malhotra <avimalhotra505@gmail.com>
2026-03-15 14:38:14 +0530

    useRef and lifting state

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,32 @@ import Header from "./Header";
 import Footer from "./Footer";
 import Course from "./Course";
 // import FetchCars from "./FetchData";
-import FetchUsers from "./Users";
+// import FetchUsers from "./Users";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
+import { useState } from "react";
 
 export default function App(){
-  
+
+  const [node,setNode]=useState(14);
+  const react={ name:"React", duration:4};
+  const angular={ name:"Angular", duration:3};
+
   return (
     <div className="container">
       <Header />
       <main>
         <h2>Main Page</h2>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo quam eaque, labore porro quis rerum veniam veritatis alias, perspiciatis accusamus error. Doloremque est beatae expedita hic fugit eaque perspiciatis debitis.</p>
+        <p>{react.name}, {react.duration}</p>
+        <p>{angular.name}, {angular.duration}</p>
+        <p>Node required: {node}</p>
         <hr />
 
-        <FetchUsers />
+        <Course name={react.name} duration={react.duration} nodeversion={node}></Course>
+        <Course name={angular.name} duration={angular.duration} nodeversion={node}></Course>
+        
 
       </main>
       <Footer name="Isha" />
--- src/Course.jsx
+++ src/Course.jsx
@@ -1,13 +1,41 @@
-export default function Course({name,duration=0,num, checkClick}){
-          
+import { useRef } from "react"
+
+export default function Course({name="",duration=0,nodeversion}){
+     
+     const ref=useRef(0);     
+     const input1=useRef("");
+     const input2=useRef("");
+     const data=[
+          {id:1, name:"swift", price:800000},
+          {id:2, name:"baleno", price:900000},
+          {id:3, name:"brezza", price:1300000},
+          {id:4, name:"grand vitara", price:1600000},
+     ];
+
+     // const total=data.reduce( (x,y)=>x + y.price,0 );
+     
+     
+     function changeRef(){
+          console.log(ref.current);
+          ref.current++;
+     }
+
+     function focusInput(){
+          input2.current.focus();
+     }
+
      return (
           <section className="card mb-3">
                <div className="card-body">
                <h2>{name} Course </h2>
                <p>Course duration: {duration}</p>
-               <p>Course details</p>
-               <p>{num}</p>
-               <button className="btn btn-primary" onClick={()=>checkClick(name)}>Click</button>
+               <p>Prerequisite: Install Node JS {nodeversion} </p>
+               <p>Course details here</p>
+               {/* <p>Initial Ref: {ref.current}, <button onClick={changeRef}>Change</button></p>
+               <hr />
+               <input type="text" ref={input1} />
+               <input type="text" ref={input2} />
+               <button onClick={focusInput}>Focus</button> */}
                </div>
           </section>
      )
